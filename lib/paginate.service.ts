d32849bd614ad4ecb01764442a4b19f1bd39ba7d
jerlysonsantos <jerlyson.santos@somosmesha.com>
2023-03-13 16:04:31 -0300

    added a best way to get total items count

--- lib/paginate.service.ts
+++ lib/paginate.service.ts
@@ -59,18 +59,18 @@ export class PaginateService {
       offset: start,
     })
 
-    const allData: any[] = await this.sequelize.query(`
-      WITH MyTable AS
+    const rowCountTable: any[] = await this.sequelize.query(`
+      WITH RowCountTable AS
       (
          SELECT ROW_NUMBER() OVER ( ORDER BY created_at DESC ) AS 'rowNumber' FROM ${modelName} WHERE deleted_at IS NULL
       )
       SELECT rowNumber
-      FROM MyTable
+      FROM RowCountTable
       ORDER BY rowNumber DESC
       LIMIT 1
    `)
 
-    const count = allData[0][0].rowNumber
+    const count = rowCountTable[0][0].rowNumber
 
     totalItems = count
     totalPages = Math.ceil(totalItems / offset)
